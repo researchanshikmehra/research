07640b7c2e83c75cb93e86c9022a7ce46041d1a5
researchanshikmehra <research.anshik.mehra@gmail.com>
2026-04-23 13:22:10 +0530

    Create setup.py for pip package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='A short description of your package',
+    url='https://github.com/researchanshikmehra/research',
+)
